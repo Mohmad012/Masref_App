5d3d20861f8c3f156bba5a512268a21ae06d255e
Mohmad012 <mohmadailsia@gmail.com>
2021-10-07 00:18:38 +0200

    fix bug

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -8,12 +8,13 @@ import {handelTitle} from '../../utils'
 import FooterHome from '../../layouts/Footer/FooterHome'
 // import {Fluid1 , Fluid2} from './Fluid';
 import SectionHeading from '../../components/SectionHeading'
+import Preloader from '../../components/Preloader'
 import SectionDemo from './SectionDemo'
 
 const HomeContainer = () => {
 
   useEffect(() => {
-    handelTitle('Home')
+    // handelTitle('Home')
   },[])
 
   // useEffect(() => {
@@ -26,6 +27,7 @@ const HomeContainer = () => {
 
     return (
       <>
+        <Preloader Title="Home" />
         <section className="demo section-padding-100-70" id="demo">
           <div className="container">
             <SectionHeading title='Creative designs' text='Choose a demo' />
