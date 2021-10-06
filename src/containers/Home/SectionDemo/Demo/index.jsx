07135422b078209a4fa807ee8a52fe0a8982ee1e
Mohmad012 <mohmadailsia@gmail.com>
2021-10-06 21:46:21 +0200

    fix bug

--- src/containers/Home/SectionDemo/Demo/index.jsx
+++ src/containers/Home/SectionDemo/Demo/index.jsx
@@ -12,9 +12,9 @@ const Demo = ({
   return (
       <div className="col-lg-4 col-md-6 col-sm-12">
           <div className="demo-item">
-              <a href={path} rel="noopener noreferrer"><img src={img} alt="demo" className="img-responsive" /></a>
+              <a href={path} target="_blank" rel="noopener noreferrer"><img src={img} alt="demo" className="img-responsive" /></a>
               <div className="preview-btn-wrapper text-center">
-                  <a href={path} rel="noopener noreferrer" className="preview-demo">
+                  <a href={path} target="_blank" rel="noopener noreferrer" className="preview-demo">
                     {checkLinkPage && Viewdemo}
                     {checkLinkNav && nameLinkNav}
                     <i className="fa fa-long-arrow-right"></i>
